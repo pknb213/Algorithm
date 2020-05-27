121dae06a70cf3a5a4d4c7299c029957500df105
CYJ <pknb213@naver.com>
2020-05-27 10:45:42 +0900

    Elasticsearch Script Update

---
+++ Test.py
@@ -0,0 +1,8 @@
+from random import choice
+arr = ["부끄", "쑥스", "꺄악"]
+arr2 = ["어제", "오늘", "내일"]
+arr3 = ["점심", "간식", "저녁"]
+arr4 = ["구리", "꾸리", "엉"]
+arr5 = ["!", "?", "!!!!"]
+
+print(choice(arr), choice(arr2), choice(arr3), "뭐 먹었", choice(arr4), choice(arr5))
