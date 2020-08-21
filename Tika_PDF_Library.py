63ad5eb385fcc8968ba3bd1be7828ace3dc64b4c
CYJ <pknb213@naver.com>
2020-08-21 18:21:06 +0900

    Update

--- Tika_PDF_Library.py
+++ Tika_PDF_Library.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 from tika import parser, language
 import re
 
-PDF_FILE_NAME = "TV 매뉴얼.pdf"#"LG-F700_UG_281.0_29_20161025.pdf"  
+PDF_FILE_NAME = "TV 매뉴얼.pdf"  #"LG-F700_UG_281.0_29_20161025.pdf"
 KEY_WORD = "음소거"
 
 parsed = parser.from_file(PDF_FILE_NAME, xmlContent=True)  # Exist ['metadata', 'content', 'status']
@@ -17,7 +17,7 @@ if parsed['status'] is not 200:
 print(parsed)
 print("Total Page : ", parsed['metadata']['xmpTPg:NPages'])
 html = BeautifulSoup(parsed['content'], 'html.parser')
-print(html.)
+print(html)
 # print(html)
 # print("=>", len(html.find_all(text=re.compile(KEY_WORD))))
 # for row in html.find_all(text=re.compile(KEY_WORD)):
