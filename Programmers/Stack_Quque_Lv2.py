93798c85d3be7dabdb58027242b5c87782d826da
CYJ <pknb213@naver.com>
2021-01-05 18:40:46 +0900

    Added Stack Queue

--- Programmers/Stack_Quque_Lv2.py
+++ Programmers/Stack_Quque_Lv2.py
@@ -19,16 +19,37 @@ prices  |   return
 """
 
 case = [1, 2, 3, 2, 3]
+case = [10000,50000,50001,49999,99999]
 
 
 def solution(prices):
     answer = []
-    for i in prices:
-        print(i)
-
+    pop_box = [prices.pop()]
+    answer.append(0)
+    while prices:
+        sec = 0
+        a = prices.pop()
+        for i in pop_box:
+            if a <= i:
+                sec += 1
+        pop_box.append(a)
+        answer.insert(0, sec)
     return answer
 
 
 print(case)
 print(solution(case))
 
+
+# todo : 위의 솔루션에서는 모르겠지만 제대로 동작이 안한다 테스트케이스 씨발 존나 적어서 뭐가 문제인지도 모르곘네
+
+def solution(prices):
+    answer = [0] * len(prices)
+
+    for i in range(len(prices)-1):
+        for j in range(i, len(prices)-1):
+            if prices[i] >prices[j]:
+                break
+            else:
+                answer[i] +=1
+    return answer
